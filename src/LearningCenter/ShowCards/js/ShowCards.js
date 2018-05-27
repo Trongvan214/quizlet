ba3758d82f4c1de46e21dc9eb2dbcbe161288cff
Trong Van <trongvan214@yahoo.com>
2018-05-27 14:35:08 -0500

    finish with learning center css

--- src/LearningCenter/ShowCards/js/ShowCards.js
+++ src/LearningCenter/ShowCards/js/ShowCards.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {  Row, Col, Button } from 'react-bootstrap';
+import {  Row, Col } from 'react-bootstrap';
 import '../css/ShowCards.css';
 
 export default class ShowCards extends Component {
